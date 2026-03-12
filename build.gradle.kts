abb86ae0da34a90e5f64aae8bdbb4775ecd932c8
calcagno92-commits <thomas.calcagno@isen.yncrea.fr>
2026-03-12 11:50:38 +0100

    A

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-    // Les versions ici dépendent de ce que tu as installé, 8.1.0 et 1.9.0 sont les standards pour ton setup Compose
     id("com.android.application") version "8.1.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
 }
